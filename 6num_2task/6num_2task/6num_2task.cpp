0a4c3672030b8d3093b1da09e6cf30986be16a4b
Alexey Brante <96073038+Fextice@users.noreply.github.com>
2024-03-28 10:41:46 +0300

    Add files via upload

---
+++ 6num_2task/6num_2task/6num_2task.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Component { // класс граф.компонента
+protected:
+    string name;
+    bool visible;
+public:
+    Component(const string& name) : name(name), visible(true) {}
+    virtual void draw() = 0;
+    virtual void setVisible(bool isVisible) {
+        visible = isVisible;
+    }
+};
+
+class Window : public Component { // класс "окно"
+private:
+    vector<Component*> components;
+public:
+    Window(const string& name) : Component(name) {}
+    void addComponent(Component* component) {
+        components.push_back(component);
+    }
+    void draw() override {
+        if (visible) {
+            cout << "Drawing Window: " << name << endl;
+            for (Component* component : components) {
+                component->draw();
+            }
+        }
+    }
+    ~Window() {
+        for (Component* component : components) {
+            delete component;
+        }
+    }
+};
+
+class Label : public Component { // класс "надпись"
+public:
+    Label(const string& name) : Component(name) {}
+    void draw() override {
+        if (visible) {
+            cout << "Drawing Label: " << name << endl;
+        }
+    }
+};
+
+class Button : public Component { // класс "кнопка"
+public:
+    Button(const string& name) : Component(name) {}
+    void draw() override {
+        if (visible) {
+            cout << "Drawing Button: " << name << endl;
+        }
+    }
+};
+
+int main() {
+    Window* mainWindow = new Window("Main Window");
+
+    Label* label = new Label("Username:");
+    Button* button = new Button("Submit");
+
+    mainWindow->addComponent(label);
+    mainWindow->addComponent(button);
+
+    mainWindow->draw();
+
+    delete mainWindow;
+
+    return 0;
+}
